751dbe05d4e013b73dacce70db506b9a327af4f6
adithyasource <adithyasrc@gmail.com>
2025-07-10 19:07:41 +0530

    feat: clean up code and can now paste images

--- extension/javascript.js
+++ extension/javascript.js
@@ -1,202 +1,220 @@
-var textarea = document.getElementById("textarea");
+// *** setting up variables
+const textArea = document.getElementById("textarea");
+const fontSidebar = document.getElementById("fontsidebar");
+const sideBar = document.getElementById("sidebar");
+const hoverChecker = document.getElementById("hoverchecker");
+const changeFont = document.getElementById("changefont");
+const pageBody = document.getElementById("body");
+const darkModeBtn = document.getElementById("darkmodebtn");
+const sideButton = document.querySelectorAll(".sidebutton");
+const dropDown = document.getElementById("dropdown");
+var fontTimer;
+var sidebarTimer;
 
-//finds saved value in textareaValue and adds it to the text area
-textarea.innerHTML = localStorage.getItem("textareaValue");
+// *** fetching data from localStorage and applying it to document
+textArea.innerHTML = localStorage.getItem("textareaValue");
 
-//listens for input in text area and then adds that text to textareaValue
-textarea.addEventListener("input", () => {
-  localStorage.setItem("textareaValue", textarea.innerHTML);
-});
+var fontIndex = localStorage.getItem("fontlocalstorage");
+var fontLocalStorage = dropDown.options[fontIndex];
+dropDown.selectedIndex = localStorage.getItem("fontlocalstorage");
+pageBody.style.fontFamily = fontLocalStorage.value;
 
-//different ui colors
-function darkmodetoggle1() {
-  textarea.style.color = "#FFFFFF";
-  body.style.backgroundColor = "#121212";
-  sidebar.style.backgroundColor = "#121212";
-  sidebar.style.outline = "1.5px solid white";
-  sidebutton.forEach((sidebutton) => {
-    sidebutton.style.color = "#FFFFFF";
-    dropdown.style.color = "#FFFFFF";
-    dropdown.style.backgroundColor = "#121212";
-    fontsidebar.style.outline = "1.5px solid #FFFFFF";
-    fontsidebar.style.backgroundColor = "#121212";
-  });
-}
-
-function darkmodetoggle2() {
-  textarea.style.color = "#121212";
-  body.style.backgroundColor = "#FFFFFF";
-  sidebar.style.backgroundColor = "#FFFFFF";
-  sidebar.style.outline = "2px solid black";
-  sidebutton.forEach((sidebutton) => {
-    sidebutton.style.color = "#121212";
-    dropdown.style.color = "#121212";
-    dropdown.style.backgroundColor = "#FFFFFF";
-    fontsidebar.style.outline = "2px solid #121212";
-    fontsidebar.style.backgroundColor = "#FFFFFF";
-  });
-}
-
-let isBlurRefresh = !JSON.parse(localStorage.getItem("isBlurRefresh"));
-
-//adds blur based on state of isBlurRefresh
-if (!isBlurRefresh) {
-  textarea.classList.add("blur");
-  var isBlur = true;
+// checks device color scheme and changes favicon color accordingly
+var favIcon = document.getElementById("favicon");
+var browserIsDark = window.matchMedia("(prefers-color-scheme: dark)");
+if (browserIsDark.matches) {
+  favIcon.href = "/images/128-light.png";
 } else {
-  var isBlur = false;
+  favIcon.href = "/images/128.png";
 }
 
-//listens for ctrl+q and adds or removes blur
+let isBlur = JSON.parse(localStorage.getItem("isBlur"));
+if (isBlur === null) {
+  isBlur = false;
+  localStorage.setItem("isBlur", false);
+}
+
+if (isBlur) {
+  textArea.classList.add("blur");
+} else {
+  textArea.classList.remove("blur");
+}
+
+let isDarkMode = JSON.parse(localStorage.getItem("isDarkMode"));
+if (isDarkMode === null) {
+  isDarkMode = false;
+  localStorage.setItem("isDarkMode", false);
+}
+
+if (isDarkMode) {
+  isDarkMode = true;
+  darkUI();
+} else {
+  isDarkMode = false;
+  lightUI();
+}
+
+// *** listening for input and updating
+textArea.addEventListener("input", () => {
+  localStorage.setItem("textareaValue", textArea.innerHTML);
+});
+
+// ctrl modifier listener
 document.addEventListener("keydown", (e) => {
-  if (e.ctrlKey && e.key == "q") {
-    if (!isBlur) {
-      textarea.classList.add("blur");
-      let isBlur = true;
-    } else if (isBlur) {
-      textarea.classList.remove("blur");
-      let isBlur = false;
+  if (e.ctrlKey) {
+    if (e.shiftKey && e.key === "Q") {
+      if (isDarkMode) {
+        lightUI();
+      } else {
+        darkUI();
+      }
+
+      isDarkMode = !isDarkMode;
+      localStorage.setItem("isDarkMode", isDarkMode);
     }
-    isBlur = !isBlur;
-  }
-  localStorage.setItem("isBlurRefresh", isBlur);
-});
 
-//listens for alt+q and changes between darkmodetoggle1 and 2
-document.addEventListener("keydown", (e) => {
-  if (e.altKey && e.key == "q") {
-    if (!isDarkMode) {
-      let isDarkMode = true;
-      darkmodetoggle1();
-    } else if (isDarkMode) {
-      let isDarkMode = false;
-      darkmodetoggle2();
+    if (e.key === "q") {
+      console.log(isBlur);
+      if (isBlur) {
+        textArea.classList.remove("blur");
+      } else {
+        textArea.classList.add("blur");
+      }
+
+      isBlur = !isBlur;
+      localStorage.setItem("isBlur", isBlur);
     }
-    isDarkMode = !isDarkMode;
-  }
-  localStorage.setItem("isDarkModeStorage", isDarkMode);
-});
 
-//listens for ctrl+s and cancels default action and tells it to use downloadtxtaction
-document.addEventListener("keydown", (e) => {
-  if (e.ctrlKey && e.key == "s") {
-    e.preventDefault();
-    downloadtxtaction();
+    if (e.key === "s") {
+      e.preventDefault();
+      downloadTxt();
+    }
   }
 });
 
-const fontsidebar = document.getElementById("fontsidebar");
-const sidebar = document.getElementById("sidebar");
-const hoverchecker = document.getElementById("hoverchecker");
-var fontsidebaractive = false;
-var timer;
-const changefont = document.getElementById("changefont");
-
-//checks if changefont button is clicked
-changefont.addEventListener("click", function () {
-  fontsidebar.style.visibility = "visible";
-  fontsidebaractive = true;
+// checks if changeFont button is clicked
+changeFont.addEventListener("click", () => {
+  fontSidebar.style.visibility = "visible";
 });
 
-//checks if the fontsidebar is being interacted with and changes
-//visibility accordingly
-fontsidebar.addEventListener("mouseover", function handleMouseOverEvent() {
-  clearTimeout(timer);
-  fontsidebar.style.visibility = "visible";
-  sidebar.style.visibility = "visible";
+// checks if the fontSidebar is being interacted with and changes visibility accordingly
+fontSidebar.addEventListener("mouseover", function handleMouseOverEvent() {
+  clearTimeout(fontTimer);
+  fontSidebar.style.visibility = "visible";
+  sideBar.style.visibility = "visible";
 });
 
-fontsidebar.addEventListener("mouseout", function handleMouseOutEvent() {
-  timer = setTimeout(function () {
-    fontsidebar.style.visibility = "hidden";
-    sidebar.style.visibility = "hidden";
+fontSidebar.addEventListener("mouseout", function handleMouseOutEvent() {
+  fontTimer = setTimeout(() => {
+    fontSidebar.style.visibility = "hidden";
+    sideBar.style.visibility = "hidden";
   }, 50);
 });
 
-//checks if a certain part of the screen is being hovered on for a period of time
-//and then makes the sidebar visible
-hoverchecker.addEventListener("mouseover", function handleMouseOverEvent() {
-  clearTimeout(timer);
-  timer2 = setTimeout(function () {
-    sidebar.style.visibility = "visible";
-    sidebar.style.animationName = "in";
+// checks if a certain part of the screen is being hovered on for a period of time and then makes the sidebar visible
+hoverChecker.addEventListener("mouseover", function handleMouseOverEvent() {
+  clearTimeout(fontTimer);
+  sidebarTimer = setTimeout(() => {
+    sideBar.style.visibility = "visible";
+    sideBar.style.animationName = "in";
   }, 300);
 });
 
-hoverchecker.addEventListener("mouseout", function handleMouseOutEvent() {
-  clearTimeout(timer2);
-  timer = setTimeout(function () {
-    fontsidebar.style.visibility = "hidden";
-    sidebar.style.animationName = "out";
-    setTimeout(function () {
-      sidebar.style.visibility = "hidden";
+hoverChecker.addEventListener("mouseout", function handleMouseOutEvent() {
+  clearTimeout(sidebarTimer);
+  fontTimer = setTimeout(() => {
+    fontSidebar.style.visibility = "hidden";
+    sideBar.style.animationName = "out";
+    setTimeout(() => {
+      sideBar.style.visibility = "hidden";
     }, 100);
   }, 50);
 });
 
-const body = document.getElementById("body");
-const darkmodebtn = document.getElementById("darkmodebtn");
-const sidebutton = document.querySelectorAll(".sidebutton");
-const dropdown = document.getElementById("dropdown");
-
-//checks state of ui and toggles it based on the current values
-isDarkModeStorage = JSON.parse(localStorage.getItem("isDarkModeStorage"));
-if (isDarkModeStorage == true) {
-  var isDarkMode = true;
-  darkmodetoggle1();
-} else {
-  var isDarkMode = false;
-}
-
-//checks if darkmode button is pressed in sidebar
-darkmodebtn.addEventListener("click", function () {
-  if (!isDarkMode) {
-    darkmodetoggle1();
+// checks if darkModeBtn is pressed in sidebar
+darkModeBtn.addEventListener("click", () => {
+  if (isDarkMode) {
+    lightUI();
   } else {
-    darkmodetoggle2();
+    darkUI();
   }
   isDarkMode = !isDarkMode;
-  localStorage.setItem("isDarkModeStorage", isDarkMode);
+  localStorage.setItem("isDarkMode", isDarkMode);
 });
 
-//function for downloading text, courtesy of filesaver (https://github.com/eligrey/FileSaver.js)
-function downloadtxtaction() {
-  var blob = new Blob([textarea.innerText], {
-    type: "text/plain;charset=utf-8",
-  });
-  saveAs(blob, "newtab.txt");
-}
-downloadtxt.addEventListener("click", function () {
-  downloadtxtaction();
+downloadTxtBtn.addEventListener("click", () => {
+  downloadTxt();
 });
 
-//checks for any changes in the font selector and saves it to localstorage
-//also remembers the index of that option
-var fontindex = localStorage.getItem("fontlocalstorage");
-var fontlocalstorage = dropdown.options[fontindex];
-
-dropdown.addEventListener("change", function () {
-  const selectedOption = dropdown.options[dropdown.selectedIndex].value;
-  localStorage.setItem("fontlocalstorage", dropdown.selectedIndex);
-  body.style.fontFamily = selectedOption;
+// checks for any changes in the font selector and saves it to localStorage also remembers the index of that option
+dropDown.addEventListener("change", () => {
+  const selectedOption = dropDown.options[dropDown.selectedIndex].value;
+  localStorage.setItem("fontlocalstorage", dropDown.selectedIndex);
+  pageBody.style.fontFamily = selectedOption;
 });
 
-dropdown.selectedIndex = localStorage.getItem("fontlocalstorage");
-body.style.fontFamily = fontlocalstorage.value;
-
-//any text pasted into it is converted into plain text
-textarea.addEventListener("paste", function (e) {
+// any text pasted into it is converted into plain text
+textArea.addEventListener("paste", (e) => {
   e.preventDefault();
-  var pastedtext = e.clipboardData.getData("text/plain");
-  document.execCommand("insertHTML", false, pastedtext);
+  var pastedText = e.clipboardData.getData("text/plain");
+  document.execCommand("insertHTML", false, pastedText);
   document.execCommand("enableObjectResizing", false, false);
 });
 
-//checks device color scheme and changes favicon color accordingly
-var favicon = document.getElementById("favicon");
-var browserIsDark = window.matchMedia("(prefers-color-scheme: dark)");
-if (browserIsDark.matches) {
-  favicon.href = "/images/128-light.png";
-} else {
-  favicon.href = "/images/128.png";
+// enables pasting of images into the div
+textArea.addEventListener("paste", async (e) => {
+  e.preventDefault();
+
+  const items = e.clipboardData.items;
+  for (const item of items) {
+    if (item.type.indexOf("image") !== -1) {
+      const blob = item.getAsFile();
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        document.execCommand("insertImage", false, event.target.result);
+      };
+      reader.readAsDataURL(blob);
+    } else if (item.type === "text/plain") {
+      const text = item.getAsString?.();
+      document.execCommand("insertHTML", false, text);
+    }
+  }
+});
+
+// ** util functions
+
+function darkUI() {
+  textArea.style.color = "#FFFFFF";
+  pageBody.style.backgroundColor = "#121212";
+  sideBar.style.backgroundColor = "#121212";
+  sideBar.style.outline = "1.5px solid white";
+  sideButton.forEach((btn) => {
+    btn.style.color = "#FFFFFF";
+    dropDown.style.color = "#FFFFFF";
+    dropDown.style.backgroundColor = "#121212";
+    fontSidebar.style.outline = "1.5px solid #FFFFFF";
+    fontSidebar.style.backgroundColor = "#121212";
+  });
+}
+
+function lightUI() {
+  textArea.style.color = "#121212";
+  pageBody.style.backgroundColor = "#FFFFFF";
+  sideBar.style.backgroundColor = "#FFFFFF";
+  sideBar.style.outline = "2px solid black";
+  sideButton.forEach((btn) => {
+    btn.style.color = "#121212";
+    dropDown.style.color = "#121212";
+    dropDown.style.backgroundColor = "#FFFFFF";
+    fontSidebar.style.outline = "2px solid #121212";
+    fontSidebar.style.backgroundColor = "#FFFFFF";
+  });
+}
+
+// function for downloading text, courtesy of filesaver (https://github.com/eligrey/FileSaver.js)
+function downloadTxt() {
+  var blob = new Blob([textArea.innerText], {
+    type: "text/plain;charset=utf-8",
+  });
+  saveAs(blob, "newtab.txt");
 }
